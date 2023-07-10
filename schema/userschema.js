dbd228c359b85062e8e318d0ce9bd1d7ddc2f76d
g19m7057 <sifisom599@gmail.com>
2023-07-10 06:10:25 +0200

    separated schema from user handler

---
+++ schema/userschema.js
@@ -0,0 +1,10 @@
+const Joi = require('joi');
+
+
+const userSchema = Joi.object().keys({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    email: Joi.string().email().required(),
+})
+
+module.exports = userSchema;
